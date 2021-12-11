c332faf992f5abbec33b32ff64d5dbc5740627ab
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2021-12-11 14:13:15 +0900

    create server

--- content-manager-app-api/index.js
+++ content-manager-app-api/index.js
@@ -1,1 +1,16 @@
-console.log('Server is running')
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+    console.log("From the code")
+})
+
+app.get("/api/resources", (req, res) => {
+    res.send("Hello Resources")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is listening on port:" + PORT)
+})
